755e97e00e96fe67106d9c0bbf9f5fb32790fb1e
abinpramudya <abinpramdya2@gmail.com>
2025-03-28 10:50:44 +0100

    initial commit

---
+++ exam/target/debug/deps/exam-526449391322e33f.d
@@ -0,0 +1,5 @@
+/home/abin/Documents/mir/course/smt_2/rust/github/RUST_exam/exam/target/debug/deps/libexam-526449391322e33f.rmeta: src/main.rs
+
+/home/abin/Documents/mir/course/smt_2/rust/github/RUST_exam/exam/target/debug/deps/exam-526449391322e33f.d: src/main.rs
+
+src/main.rs:
---
+++ exam/target/debug/deps/exam-c30f7c44ae57aede.d
@@ -0,0 +1,5 @@
+/home/abin/Documents/mir/course/smt_2/rust/github/RUST_exam/exam/target/debug/deps/libexam-c30f7c44ae57aede.rmeta: src/main.rs
+
+/home/abin/Documents/mir/course/smt_2/rust/github/RUST_exam/exam/target/debug/deps/exam-c30f7c44ae57aede.d: src/main.rs
+
+src/main.rs:
