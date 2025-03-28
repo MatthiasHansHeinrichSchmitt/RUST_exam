786648930be388192f8897ae3117541f5a1c25df
abinpramudya <abinpramdya2@gmail.com>
2025-03-28 11:14:43 +0100

    added target

---
+++ exam/target/debug/deps/exam-b0ad98b0a0eb8d5b.d
@@ -0,0 +1,5 @@
+/home/abin/Documents/mir/course/smt_2/rust/github/RUST_exam/exam/target/debug/deps/exam-b0ad98b0a0eb8d5b: src/main.rs
+
+/home/abin/Documents/mir/course/smt_2/rust/github/RUST_exam/exam/target/debug/deps/exam-b0ad98b0a0eb8d5b.d: src/main.rs
+
+src/main.rs:
---
+++ exam/target/debug/exam.d
@@ -0,0 +1,1 @@
+/home/abin/Documents/mir/course/smt_2/rust/github/RUST_exam/exam/target/debug/exam: /home/abin/Documents/mir/course/smt_2/rust/github/RUST_exam/exam/src/main.rs
